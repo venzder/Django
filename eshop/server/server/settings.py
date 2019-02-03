d818fa8f581ca8cc89a721b4027263b664dfbecf
Sergey Ivanov <sergivanov@list.ru>
2019-02-04 01:43:04 +0300

    Создал master.css и привязал его к проекту.

--- eshop/server/server/settings.py
+++ eshop/server/server/settings.py
@@ -129,3 +129,7 @@ MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'server', 'static')
+]
