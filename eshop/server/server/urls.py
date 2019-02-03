a26e2d669cd1dce3039258a8270dd08793ff9513
Sergey Ivanov <sergivanov@list.ru>
2019-02-03 18:04:55 +0300

    Собрал статику в одном месте

--- eshop/server/server/urls.py
+++ eshop/server/server/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
@@ -22,3 +24,6 @@ urlpatterns = [
     path('', include('main.urls')),
     path('products/', include('products.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
