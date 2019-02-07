3841c03e89a4aa50781d72447b12304cdab62aa8
Sergey Ivanov <sergivanov@list.ru>
2019-02-07 22:57:47 +0300

    Создал миграции, перенес данные в базу данных, прочитал данные и поправил шаблон продукта.

--- eshop/server/products/urls.py
+++ eshop/server/products/urls.py
@@ -5,6 +5,6 @@ from .views import (
 
 
 urlpatterns = [
-    path('', product_list_view),
-    path('<int:idx>/', product_detail_view),
+    path('', product_list_view, name='list'),
+    path('<int:pk>/', product_detail_view, name='detail'),
 ]
