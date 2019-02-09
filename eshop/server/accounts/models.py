19d3cee640b58dcee230ff36e2320ed786852065
Sergey Ivanov <sergivanov@list.ru>
2019-02-09 23:51:20 +0300

    сделал преобразование базы данных и создал суперпользователя.

--- eshop/server/accounts/models.py
+++ eshop/server/accounts/models.py
@@ -1,2 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+
+
+class AccountUser(AbstractUser):
+    avatar = models.ImageField(upload_to='accounts', blank=True, null=True)
+    phone = models.CharField(max_length=13, blank=True, null=True)
+
+    def __str__(self):
+        return self.username
